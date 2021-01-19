4e9503f257b0f5af10ff0d9849cf2fa43d4307fd
Arunava Banerjee <Arunava Banerjee@DESKTOP-RBAIMPE>
2021-01-19 06:03:30 -0800

    Channel Implementation

---
+++ MyChannel/ChannelData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyChannel
+{
+    class ChannelData<T>
+    {
+        private ConcurrentQueue<T> _q = new ConcurrentQueue<T>();
+
+        private SemaphoreSlim _s = new SemaphoreSlim(0);
+
+
+        public void Write(T item)
+        {
+            _q.Enqueue(item);
+            Console.WriteLine("Producer stores " + item);
+            _s.Release();
+
+        }
+
+
+        public async Task<T> Read()
+        {
+            await _s.WaitAsync();
+
+            _q.TryDequeue(out T i);
+            Console.WriteLine("Consumer reads " + i);
+
+            return i;
+
+
+        }
+
+
+
+
+
+    }
+}
--- MyChannel/Program.cs
+++ MyChannel/Program.cs
@@ -8,8 +8,47 @@ namespace MyChannel
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+
+
+            ChannelData<int> channelData = new ChannelData<int>();
+
+            // Producer 
+
+          Task t = Task.Factory.StartNew( async () =>
+            {
+                for (int i = 0; ; i++)
+                {
+                    {
+                        await Task.Delay(1000);
+                        channelData.Write(i);
+
+                    }
+                }
+
+
+            }
+
+
+            );
+              
+
+
+
+
+
+            // Consumer
+
+            while(1==1)
+            {
+                Console.WriteLine(await channelData.Read() );
+            }
+
+
+
+
+
         }
     }
 }
